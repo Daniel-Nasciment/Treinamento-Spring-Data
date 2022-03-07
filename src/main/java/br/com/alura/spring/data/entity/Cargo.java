41c1d384459710d754997d9a128239ad59950a9e
Daniel <daniel.fecaf@outlook.com>
2022-03-07 11:05:10 -0300

    Definindo controller e entidade para cargo

---
+++ src/main/java/br/com/alura/spring/data/entity/Cargo.java
@@ -0,0 +1,27 @@
+package br.com.alura.spring.data.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cargos")
+public class Cargo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+}
