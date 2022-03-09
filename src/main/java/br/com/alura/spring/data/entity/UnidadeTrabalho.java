08b9b78196479d51cce7196f23cc30291ce27720
Daniel <daniel.fecaf@outlook.com>
2022-03-09 11:04:03 -0300

    Adicionando @Id na unidade de trabalho

--- src/main/java/br/com/alura/spring/data/entity/UnidadeTrabalho.java
+++ src/main/java/br/com/alura/spring/data/entity/UnidadeTrabalho.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
@@ -12,6 +15,8 @@ import javax.persistence.Table;
 @Table(name = "unidades_trabalho")
 public class UnidadeTrabalho {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	private String descricao;
